a1343f849ea3e6c0bb0cdaf20c1d80dfb4c35c06
jiangli <1254914148@qq.com>
2020-08-19 19:08:46 +0800

    ddd

---
+++ Runtime/DebugMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DebugMessage
+{
+    public class DebugMessage : MonoBehaviour
+    {
+        /// <summary>
+        /// 打印信息
+        /// </summary>
+        /// <param name="message">需要打印的字符</param>
+        public void Debug(string message)
+        {
+            UnityEngine.Debug.Log("这是第一个版本的打印，打印的字符为：  " + message);
+        }
+    }
+}
+
